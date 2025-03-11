06bde48c29743b67bed5eaaef56065d711be3c9f
HusseinMassalkhi55 <halmassalkhi@sabis.net>
2025-03-11 10:48:07 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -1,11 +1,13 @@
 const BlockType = require('../../extension-support/block-type');
 const ArgumentType = require('../../extension-support/argument-type');
 const TargetType = require('../../extension-support/target-type');
+const fetch = require('node-fetch');
 
 class Scratch3YourExtension {
 
     constructor (runtime) {
-        // put any setup for your extension here
+        this.apiKey = 'acc_915f79d5ee57c37';
+        this.apiSecret = '9e78e45c5aec06c69fbf00c85a748102';
     }
 
     /**
@@ -16,68 +18,29 @@ class Scratch3YourExtension {
             // unique ID for your extension
             id: 'yourScratchExtension',
 
-            // name that will be displayed in the Scratch UI
+            // name displayed in the Scratch UI
             name: 'Demo',
 
             // colours to use for your extension blocks
             color1: '#000099',
             color2: '#660066',
 
-            // icons to display
-            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
-
             // your Scratch blocks
             blocks: [
                 {
-                    // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
+                    // function where your code logic lives
+                    opcode: 'classifyImage',
 
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                    // type of block
                     blockType: BlockType.REPORTER,
 
                     // label to display on the block
-                    text: 'My first block [MY_NUMBER] and [MY_STRING]',
-
-                    // true if this block should end a stack
-                    terminal: false,
-
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    filter: [ TargetType.SPRITE, TargetType.STAGE ],
+                    text: 'Classify image [IMAGE_URL]',
 
                     // arguments used in the block
                     arguments: {
-                        MY_NUMBER: {
-                            // default value before the user sets something
-                            defaultValue: 123,
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        },
-                        MY_STRING: {
-                            // default value before the user sets something
-                            defaultValue: 'hello',
-
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
+                        IMAGE_URL: {
+                            defaultValue: 'https://example.com/image.jpg',
                             type: ArgumentType.STRING
                         }
                     }
@@ -86,14 +49,29 @@ class Scratch3YourExtension {
         };
     }
 
-
     /**
-     * implementation of the block with the opcode that matches this name
-     *  this will be called when the block is used
+     * implementation of the block to classify an image
      */
-    myFirstBlock ({ MY_NUMBER, MY_STRING }) {
-        // example implementation to return a string
-        return MY_STRING + ' : doubled would be ' + (MY_NUMBER * 2);
+    classifyImage ({ IMAGE_URL }) {
+        const url = `https://api.imagga.com/v2/tags?image_url=${encodeURIComponent(IMAGE_URL)}`;
+        const headers = {
+            'Authorization': 'Basic ' + Buffer.from(`${this.apiKey}:${this.apiSecret}`).toString('base64')
+        };
+
+        return fetch(url, { headers })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result && data.result.tags && data.result.tags.length > 0) {
+                    const tag = data.result.tags[0];
+                    return `${tag.tag.en} (${(tag.confidence).toFixed(2)}%)`;
+                } else {
+                    return 'No tags found';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                return 'Error occurred';
+            });
     }
 }
 
